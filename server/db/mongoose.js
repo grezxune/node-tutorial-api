44d191facdc4f8fc696a950d5cd31e759c4b62ff
Trzebiatowski <trzebiat@mfldclin.org>
2017-06-06 14:15:00 -0500

    setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp').catch((err) => console.log('Failed to connect to MongoDB server'));
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp').catch((err) => console.log('Failed to connect to MongoDB server'));
 
 module.exports = {
   mongoose: mongoose
