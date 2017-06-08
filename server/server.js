6283734bf2afd255229bcd18e39ee2043e722117
Trzebiatowski <trzebiat@mfldclin.org>
2017-06-08 16:00:55 -0500

    add get users/me and authentication middleware

--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ const port = process.env.PORT;
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 
@@ -126,6 +127,10 @@ app.get('/users', (req, res) => {
   User.find().then((users) => {
     res.send({users});
   }).catch((err) => res.status(400).send());
+});
+
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user);
 });
 
 app.listen(port, () => {
